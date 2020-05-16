9af6426ce6583d0877622b83645b8a35c14aba68
Albi Mrázová <mrazova.albina@gmail.com>
2020-05-16 12:46:19 +0200

    fixed error handling

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -4,25 +4,28 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
-export class ErrorInterceptor implements HttpInterceptor {
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+export class ErrorInterceptor implements HttpInterceptor{
+    intercept(
+        req: import('@angular/common/http').HttpRequest<any>,
+        next: import('@angular/common/http').HttpHandler
+        ): import('rxjs')
+    .Observable<import('@angular/common/http').HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error => {
-                if (error instanceof HttpErrorResponse) {
-                    if (error.status === 401) {
-                        return throwError(error.statusText);
-                    }
+                if (error.status === 401){
+                    return throwError(error.statusText);
+                }
+                if (error instanceof HttpErrorResponse){
                     const applicationError = error.headers.get('Application-Error');
-                    if (applicationError) {
-                        console.error(applicationError);
+                    if (applicationError){
                         return throwError(applicationError);
                     }
                     const serverError = error.error;
                     let modalStateErrors = '';
-                    if (serverError && typeof serverError === 'object') {
-                        for (const key in serverError) {
-                            if (serverError[key]) {
-                                modalStateErrors += serverError[key] + '\n';
+                    if (serverError.errors && typeof serverError.errors === 'object'){
+                        for (const key in serverError.errors){
+                            if (serverError.errors[key]){
+                                modalStateErrors += serverError.errors[key] + '\n';
                             }
                         }
                     }
